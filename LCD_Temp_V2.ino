c7c9f8536312c0987822da1209e7368a08b1af36
SynxEU <126554945+SynxEU@users.noreply.github.com>
2025-05-26 11:01:17 +0200

    Update LCD_Temp_V2.ino

--- LCD_Temp_V2.ino
+++ LCD_Temp_V2.ino
@@ -1,27 +1,42 @@
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
+// Temp
 int Vo;
 float R1 = 10000;
 float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 int cel;
-int currentRed = 0;
-int currentGreen = 0;
-int currentBlue = 0;
 
-#define FADE_SPEED 100
-#define RED_LIGHT_PIN 6
-#define GREEN_LIGHT_PIN 9
-#define BLUE_LIGHT_PIN 10
+// RGB
+int currentRed = 0, currentGreen = 0, currentBlue = 0;
+
+// Servo
+Servo myServo;
+int LDRValue = 0, LDRAfter = 0, lightReadings = 20, lux = 0;
+
+// Pointers
+int *ptrLux = &lux;
+int *ptrLightReading = &lightReadings;
+
+// Pins used
 #define THERMISTOR_PIN A0
+#define LDR A4
 #define BRIGHTNESS_PIN A5
+#define RED_LIGHT_PIN 6
+#define GREEN_LIGHT_PIN 5
+#define BLUE_LIGHT_PIN 3
 #define RS 12
-#define EN 11
-#define D4 5
+#define EN 7
+#define D4 8
 #define D5 4
-#define D6 3
+#define D6 11
 #define D7 2
 
+// Other defines
+#define FADE_SPEED 100
+
+// LCD
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 void setup() {
@@ -29,12 +44,15 @@ void setup() {
   pinMode(RED_LIGHT_PIN, OUTPUT);
   pinMode(BLUE_LIGHT_PIN, OUTPUT);
   pinMode(THERMISTOR_PIN, INPUT);
+  myServo.attach(9);
+  pinMode(LDR, INPUT);
   Serial.begin(9600);
   lcd.begin(16, 2);
 }
 
 void loop() {
 
+  // Temp beregninger
   Vo = analogRead(THERMISTOR_PIN);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
   logR2 = log(R2);
@@ -43,16 +61,38 @@ void loop() {
   T = (T * 9.0)/ 5.0 + 32.0; 
   cel = (int)((T-30)/2);
 
+  // Servo
+  servoControl(&lightReadings);
+  lightReadings++;
+  
+  // LCD
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Light: ");lcd.print(constrain(lux, 0, 880));lcd.print(" LUX");
+
   lcd.setCursor(0, 1);
-
-  setLEDColor(cel);
-
-  lcd.setCursor(0, 2);
   lcd.print("TEMP: ");lcd.print(cel);lcd.print((char)0b11011111);lcd.print("C");
+
+  // LED RGB
+  setLEDColor(cel);
 
   delay(300);
 }
 
+void servoControl(int *readings) {
+  if (*readings == 20) {
+    LDRValue = analogRead(LDR);
+    LDRAfter = map(LDRValue, 0, 1023, 0, 180);
+    LDRAfter = constrain(LDRAfter, 0, 180);
+
+    *ptrLightReading = 0;
+
+    *ptrLux = map(LDRValue, 0, 1023, 0, 880);
+
+    myServo.write(LDRAfter);
+  }
+}
 
 void setLEDColor(int tempC) {
   int targetRed, targetGreen, targetBlue;
@@ -61,20 +101,14 @@ void setLEDColor(int tempC) {
     targetRed = 0;
     targetGreen = 0;
     targetBlue = 255;
-    lcd.clear();
-    lcd.print("Too Cold!");
   } else if (tempC > 28) {
     targetRed = 255;
     targetGreen = 0;
     targetBlue = 0;
-    lcd.clear();
-    lcd.print("Too Hot!");
   } else {
     targetRed = 0;
     targetGreen = 255;
     targetBlue = 0;
-    lcd.clear();
-    lcd.print("Perfect Temp");
   }
 
   currentRed = fadeTo(currentRed, targetRed);
